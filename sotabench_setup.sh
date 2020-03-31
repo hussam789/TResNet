563253ba827b892655d7a370216180b94edd38f8
hussam789 <hussam789@gmail.com>
2020-04-01 00:47:40 +0300

    Update sotabench_setup.sh

--- sotabench_setup.sh
+++ sotabench_setup.sh
@@ -9,3 +9,4 @@ $PYTHON -m pip install torch>=1.3
 $PYTHON -m pip install torchvision>=0.4.0
 $PYTHON -m pip install git+https://github.com/mapillary/inplace_abn.git@v1.0.11
 $PYTHON -m pip install gdown
+gdown https://drive.google.com/uc?id=12_VnXYI-4JaUYOOIsZXYCJdpiLCJ4dHV
