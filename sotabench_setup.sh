0e0727a1c48105751c783c9c65fa517fd79c4add
hussam789 <hussam789@gmail.com>
2020-04-01 00:19:24 +0300

    Update sotabench_setup.sh

--- sotabench_setup.sh
+++ sotabench_setup.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/env bash
-pip install torch>=1.3
-pip install torchvision>=0.4.0
-pip install git+https://github.com/mapillary/inplace_abn.git@v1.0.11
+#!/usr/bin/env bash -x
+source /workspace/venv/bin/activate
+PYTHON=${PYTHON:-"python"}
+# REPO="$( cd "$(dirname "$0")" ; cd .. ; pwd -P )"
+# cd $REPO
+
+$PYTHON -m pip install -e .
+$PYTHON -m pip install torch>=1.3
+$PYTHON -m pip install torchvision>=0.4.0
+$PYTHON -m pip install git+https://github.com/mapillary/inplace_abn.git@v1.0.11
