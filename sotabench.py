df7a141aaf719e6a8f79edca8be6e74e100e3ea4
hussam789 <hussam789@gmail.com>
2020-04-01 21:34:43 +0300

    Update sotabench.py

--- sotabench.py
+++ sotabench.py
@@ -129,3 +129,38 @@ ImageNet.benchmark(
     model_description="Official weights from the author's of the paper."
 )
 torch.cuda.empty_cache()
+
+
+# TResNet-M-448
+args.model_name = 'tresnet_xl'
+model_path = './tresnet_m_448.pth'
+model = create_model(args)
+state = torch.load(model_path, map_location='cpu')['model']
+model.load_state_dict(state, strict=True)
+model = InplacABN_to_ABN(model)
+model = fuse_bn_recursively(model)
+model = model.cuda()
+model.eval()
+
+val_bs = args.batch_size
+val_tfms = transforms.Compose(
+    [transforms.Resize(int(args.input_size / args.val_zoom_factor)),
+     transforms.CenterCrop(args.input_size)])
+val_tfms.transforms.append(transforms.ToTensor())
+    
+print('Benchmarking TResNet-M 448')
+
+# Run the benchmark
+ImageNet.benchmark(
+    model=model,
+    paper_model_name='TResNet-M (input=448)',
+    paper_arxiv_id='2003.13630',
+    input_transform=val_tfms,
+    batch_size=212,
+    num_workers=args.num_workers,
+    num_gpu=1,
+    pin_memory=True,
+    paper_results={'Top 1 Accuracy': 0.832},
+    model_description="Official weights from the author's of the paper."
+)
+torch.cuda.empty_cache()
