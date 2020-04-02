8a7cbff82fe3235a858d6cfd848c9e0b17d4257b
hussam789 <hussam789@gmail.com>
2020-04-02 03:56:07 +0300

    Update sotabench.py

--- sotabench.py
+++ sotabench.py
@@ -87,7 +87,7 @@ ImageNet.benchmark(
     paper_model_name='TResNet-M (288-Mean-Max)',
     paper_arxiv_id='2003.13630',
     input_transform=val_tfms,
-    batch_size=250,
+    batch_size=432,
     num_workers=args.num_workers,
     num_gpu=1,
     pin_memory=True,
@@ -122,6 +122,45 @@ print('Benchmarking TResNet-L')
 ImageNet.benchmark(
     model=model,
     paper_model_name='TResNet-L',
+    paper_arxiv_id='2003.13630',
+    input_transform=val_tfms,
+    batch_size=250,
+    num_workers=args.num_workers,
+    num_gpu=1,
+    pin_memory=True,
+    paper_results={'Top 1 Accuracy': 0.814, 'Top 5 Accuracy': 0.956},
+    model_description="Official weights from the author's of the paper."
+)
+
+del model
+gc.collect()
+torch.cuda.empty_cache()
+
+# LTResNet 288-Mean-Max
+
+val_bs = args.batch_size
+val_tfms = transforms.Compose(
+    [transforms.Resize(288),
+     transforms.CenterCrop(288)])
+val_tfms.transforms.append(transforms.ToTensor())
+
+model_path = './tresnet_l.pth'
+model = create_model(args)
+state = torch.load(model_path, map_location='cpu')['model']
+model.load_state_dict(state, strict=True)
+
+model = TestTimePoolHead(model)
+
+model = InplacABN_to_ABN(model)
+model = fuse_bn_recursively(model)
+model = model.cuda()
+model.eval()
+print('Benchmarking TResNet-L (288-Mean-Max)')
+
+# Run the benchmark
+ImageNet.benchmark(
+    model=model,
+    paper_model_name='TResNet-L (288-Mean-Max)',
     paper_arxiv_id='2003.13630',
     input_transform=val_tfms,
     batch_size=250,
@@ -173,6 +212,45 @@ del model
 gc.collect()
 torch.cuda.empty_cache()
 
+# XLTResNet 288-Mean-Max
+
+val_bs = args.batch_size
+val_tfms = transforms.Compose(
+    [transforms.Resize(288),
+     transforms.CenterCrop(288)])
+val_tfms.transforms.append(transforms.ToTensor())
+
+model_path = './tresnet_xl.pth'
+model = create_model(args)
+state = torch.load(model_path, map_location='cpu')['model']
+model.load_state_dict(state, strict=True)
+
+model = TestTimePoolHead(model)
+
+model = InplacABN_to_ABN(model)
+model = fuse_bn_recursively(model)
+model = model.cuda()
+model.eval()
+print('Benchmarking TResNet-XL (288-Mean-Max)')
+
+# Run the benchmark
+ImageNet.benchmark(
+    model=model,
+    paper_model_name='TResNet-XL (288-Mean-Max)',
+    paper_arxiv_id='2003.13630',
+    input_transform=val_tfms,
+    batch_size=250,
+    num_workers=args.num_workers,
+    num_gpu=1,
+    pin_memory=True,
+    paper_results={'Top 1 Accuracy': 0.820, 'Top 5 Accuracy': 0.959},
+    model_description="Official weights from the author's of the paper."
+)
+
+del model
+gc.collect()
+torch.cuda.empty_cache()
+
 # TResNet-M-448
 args.model_name = 'tresnet_m'
 model_path = './tresnet_m_448.pth'
@@ -196,6 +274,43 @@ print('Benchmarking TResNet-M 448')
 ImageNet.benchmark(
     model=model,
     paper_model_name='TResNet-M (input=448)',
+    paper_arxiv_id='2003.13630',
+    input_transform=val_tfms,
+    batch_size=125,
+    num_workers=args.num_workers,
+    num_gpu=1,
+    pin_memory=True,
+    paper_results={'Top 1 Accuracy': 0.832},
+    model_description="Official weights from the author's of the paper."
+)
+
+del model
+gc.collect()
+torch.cuda.empty_cache()
+
+# TResNet-M-512-Mean-Max
+args.model_name = 'tresnet_m'
+model_path = './tresnet_m_448.pth'
+args.input_size = 512
+model = create_model(args)
+state = torch.load(model_path, map_location='cpu')['model']
+model.load_state_dict(state, strict=True)
+model = InplacABN_to_ABN(model)
+model = fuse_bn_recursively(model)
+model = model.cuda()
+model.eval()
+
+val_bs = args.batch_size
+val_tfms = transforms.Compose(
+    [transforms.Resize((args.input_size, args.input_size))])
+val_tfms.transforms.append(transforms.ToTensor())
+    
+print('Benchmarking TResNet-M 512-mean-max')
+
+# Run the benchmark
+ImageNet.benchmark(
+    model=model,
+    paper_model_name='TResNet-M (512-mean-max)',
     paper_arxiv_id='2003.13630',
     input_transform=val_tfms,
     batch_size=125,
