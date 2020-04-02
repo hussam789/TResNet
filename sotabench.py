4fd69e8a5148c7a6c7f91f229eb71dbde3e2416c
hussam789 <hussam789@gmail.com>
2020-04-02 05:26:02 +0300

    Update sotabench.py

--- sotabench.py
+++ sotabench.py
@@ -252,41 +252,41 @@ gc.collect()
 torch.cuda.empty_cache()
 
 # TResNet-M-448
-args.model_name = 'tresnet_m'
-model_path = './tresnet_m_448.pth'
-args.input_size = 448
-model = create_model(args)
-state = torch.load(model_path, map_location='cpu')['model']
-model.load_state_dict(state, strict=True)
-model = InplacABN_to_ABN(model)
-model = fuse_bn_recursively(model)
-model = model.cuda()
-model.eval()
+# args.model_name = 'tresnet_m'
+# model_path = './tresnet_m_448.pth'
+# args.input_size = 448
+# model = create_model(args)
+# state = torch.load(model_path, map_location='cpu')['model']
+# model.load_state_dict(state, strict=True)
+# model = InplacABN_to_ABN(model)
+# model = fuse_bn_recursively(model)
+# model = model.cuda()
+# model.eval()
 
-val_bs = args.batch_size
-val_tfms = transforms.Compose(
-    [transforms.Resize((args.input_size, args.input_size))])
-val_tfms.transforms.append(transforms.ToTensor())
+# val_bs = args.batch_size
+# val_tfms = transforms.Compose(
+#     [transforms.Resize((args.input_size, args.input_size))])
+# val_tfms.transforms.append(transforms.ToTensor())
     
-print('Benchmarking TResNet-M 448')
+# print('Benchmarking TResNet-M 448')
 
-# Run the benchmark
-ImageNet.benchmark(
-    model=model,
-    paper_model_name='TResNet-M (input=448)',
-    paper_arxiv_id='2003.13630',
-    input_transform=val_tfms,
-    batch_size=125,
-    num_workers=args.num_workers,
-    num_gpu=1,
-    pin_memory=True,
-    paper_results={'Top 1 Accuracy': 0.832},
-    model_description="Official weights from the author's of the paper."
-)
+# # Run the benchmark
+# ImageNet.benchmark(
+#     model=model,
+#     paper_model_name='TResNet-M (input=448)',
+#     paper_arxiv_id='2003.13630',
+#     input_transform=val_tfms,
+#     batch_size=125,
+#     num_workers=args.num_workers,
+#     num_gpu=1,
+#     pin_memory=True,
+#     paper_results={'Top 1 Accuracy': 0.832},
+#     model_description="Official weights from the author's of the paper."
+# )
 
-del model
-gc.collect()
-torch.cuda.empty_cache()
+# del model
+# gc.collect()
+# torch.cuda.empty_cache()
 
 # TResNet-M-512-Mean-Max
 args.model_name = 'tresnet_m'
