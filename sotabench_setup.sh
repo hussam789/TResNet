2e56c909838bde982a906d70963509d575cfe91c
mrT23 <talrid@gmail.com>
2020-08-26 11:57:37 +0300

    Update sotabench_setup.sh
    torchvision==0.5.0

--- sotabench_setup.sh
+++ sotabench_setup.sh
@@ -7,7 +7,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 $PYTHON -m pip install -e .
 $PYTHON -m pip install torch==1.4.0
-$PYTHON -m pip install torchvision==0.4.0
+$PYTHON -m pip install torchvision==0.5.0
 $PYTHON -m pip install git+https://github.com/mapillary/inplace_abn.git@v1.0.12
 $PYTHON -m pip install gdown
 $PYTHON -m pip install --upgrade pip
