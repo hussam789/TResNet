5d36017d6121c17023ba427a547b9c09ea2aa929
mrT23 <talrid@gmail.com>
2020-08-27 14:50:45 +0300

    Update sotabench_setup.sh

--- sotabench_setup.sh
+++ sotabench_setup.sh
@@ -27,7 +27,6 @@ $PYTHON -m pip install --upgrade Pillow
 #python -c "from PIL import Image; print(Image.PILLOW_VERSION)"
 
 gdown https://drive.google.com/uc?id=1pySDEdfvLRROaNXi-fWrd_aV9N6W9Yjo
-
 gdown https://drive.google.com/uc?id=12_VnXYI-4JaUYOOIsZXYCJdpiLCJ4dHV
 gdown https://drive.google.com/uc?id=14X7xL1uf3c9dVGzRdV49bTjCBNvT9koA
 gdown https://drive.google.com/uc?id=1HKFZEWPIwk5ZR0_MjJNkpQLyM_0yX0Qq
