58fbc3fbe8c51cc7ecb6d7d0f41540242e927b3c
mrT23 <talrid@gmail.com>
2020-12-12 16:49:21 +0200

    Update sotabench.py
    tst if sotabench still working

--- sotabench.py
+++ sotabench.py
@@ -60,67 +60,36 @@ del model
 gc.collect()
 torch.cuda.empty_cache()
 
-# #### TResNet-L-2 ####
-args.model_name = 'tresnet_l_v2'
-model_path = './tresnet_l_2.pth'
-model = create_model(args)
-state = torch.load(model_path, map_location='cpu')['model']
-model.load_state_dict(state, strict=True)
-model = InplacABN_to_ABN(model)
-model = fuse_bn_recursively(model)
-model = model.cuda()
-model = model.half()
-model.eval()
+# # #### TResNet-L-2 ####
+# args.model_name = 'tresnet_l_v2'
+# model_path = './tresnet_l_2.pth'
+# model = create_model(args)
+# state = torch.load(model_path, map_location='cpu')['model']
+# model.load_state_dict(state, strict=True)
+# model = InplacABN_to_ABN(model)
+# model = fuse_bn_recursively(model)
+# model = model.cuda()
+# model = model.half()
+# model.eval()
 
-# Run the benchmark
-print('Benchmarking TResNet-L-V2-FP16 ')
-for i in range(1):  # Two times for caching
-    ImageNet.benchmark(
-        model=model,
-        paper_model_name='TResNet-L-V2-FP16',
-        paper_arxiv_id='2003.13630',
-        input_transform=val_tfms,
-        batch_size=600,
-        num_workers=args.num_workers,
-        num_gpu=1,
-        pin_memory=True,
-        paper_results={'Top 1 Accuracy': 0.819, 'Top 5 Accuracy': 0.951},
-        model_description="TResNet-L-V2.",
-        send_data_to_device=upload_data_to_gpu,
-    )
+# # Run the benchmark
+# print('Benchmarking TResNet-L-V2-FP16 ')
+# for i in range(1):  # Two times for caching
+#     ImageNet.benchmark(
+#         model=model,
+#         paper_model_name='TResNet-L-V2-FP16',
+#         paper_arxiv_id='2003.13630',
+#         input_transform=val_tfms,
+#         batch_size=600,
+#         num_workers=args.num_workers,
+#         num_gpu=1,
+#         pin_memory=True,
+#         paper_results={'Top 1 Accuracy': 0.819, 'Top 5 Accuracy': 0.951},
+#         model_description="TResNet-L-V2.",
+#         send_data_to_device=upload_data_to_gpu,
+#     )
 
-del model
-gc.collect()
-torch.cuda.empty_cache()
+# del model
+# gc.collect()
+# torch.cuda.empty_cache()
 
-#### TResNet-M ####
-args.model_name = 'tresnet_m'
-model_path = './tresnet_m.pth'
-model = create_model(args)
-state = torch.load(model_path, map_location='cpu')['model']
-model.load_state_dict(state, strict=True)
-model = InplacABN_to_ABN(model)
-model = fuse_bn_recursively(model)
-model = model.cuda()
-model = model.half()
-model.eval()
-
-print('Benchmarking TResNet-M')
-for i in range(1):  # Two times for caching
-    ImageNet.benchmark(
-        model=model,
-        paper_model_name='TResNet-M-FP16',
-        paper_arxiv_id='2003.13630',
-        input_transform=val_tfms,
-        batch_size=640,
-        num_workers=args.num_workers,
-        num_gpu=1,
-        pin_memory=True,
-        paper_results={'Top 1 Accuracy': 0.807, 'Top 5 Accuracy': 0.948},
-        model_description="Official weights from the author's of the paper.",
-        send_data_to_device=upload_data_to_gpu
-    )
-
-del model
-gc.collect()
-torch.cuda.empty_cache()
